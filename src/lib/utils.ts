0bdb3dcad907ec89034cbaa95301d198375ba5a5
uzair223 <uzairpatel223@outlook.com>
2026-02-24 17:28:34 +0000

    fix bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -62,8 +62,8 @@ export function getRoleLabel(role: string) {
 
 export const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/";
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`;
